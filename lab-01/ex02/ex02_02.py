bf57452a5b085a61418c5cb79a0665d7e3750ae5
tsennguyen <vietthanhnguyen.tsen@gmail.com>
2025-02-28 15:47:36 +0700

    fix

--- lab-01/ex02/ex02_02.py
+++
@@ -1,4 +0,0 @@
-ban_kinh = float(input("Nhap ban kinh cua hinh tron: "))
-
-dientich = 3.14 * ban_kinh * ban_kinh
-print("Dien tich cua hinh tron la: ", dientich)
--- lab-01/ex02_03.py
+++
@@ -1,7 +0,0 @@
-#Nhap so tu nguoi dung
-so = int(input("Nhap so nguyen: "))
-
-if so % 2 == 0:
-    print("So ban vua nhap la so chan")
-else:
-    print("So ban vua nhap la so le")
